f854cc15517046813a2c4bcdb60efee091d38d24
Marco <marco.ceccarelli.code@gmail.com>
2024-07-09 16:40:39 +0200

    order and gitignore

--- philo/src/initialize.c
+++ philo/src/initialize.c
@@ -12,26 +12,7 @@
 
 #include "philo.h"
 
-// Just left node in parameters becuase rigth creation is used
-//	=> assign just the left one because you don't know what will be later
-static t_philo	*new_philo(t_data *data, int id, t_philo *left_philo)
-{
-	t_philo	*philo;
-
-	philo = malloc(sizeof(t_philo));
-	if (!philo)
-		free_and_exit(data, "Error\n allocation failed");
-	philo->id = id;
-	philo->left_philo = left_philo;
-	philo->time_to_die = data->time_to_die;
-	philo->time_to_eat = data->time_to_eat;
-	philo->time_to_sleep = data->time_to_sleep;
-	philo->birthday = get_current_time();
-	philo->last_meal = get_current_time();
-	philo->meals_eaten = 0;
-	philo->write_lock = data->write_lock;
-	return (philo);
-}
+static t_philo	*new_philo(t_data *data, int id, t_philo *left_philo);
 
 // Used to allocate philosophers and forks
 // i starts from 1 beacause philos id must start from 1
@@ -62,3 +43,25 @@ t_data	*initialize_table(t_data *data)
 	data->first_philo->left_philo = philo_tmp;
 	return (data);
 }
+
+// Just left node in parameters becuase rigth creation is used
+//	=> assign just the left one because you don't know what will be later
+static t_philo	*new_philo(t_data *data, int id, t_philo *left_philo)
+{
+	t_philo	*philo;
+
+	philo = malloc(sizeof(t_philo));
+	if (!philo)
+		free_and_exit(data, "Error\n allocation failed");
+	philo->id = id;
+	philo->left_philo = left_philo;
+	philo->time_to_die = data->time_to_die;
+	philo->time_to_eat = data->time_to_eat;
+	philo->time_to_sleep = data->time_to_sleep;
+	philo->birthday = get_current_time();
+	philo->last_meal = get_current_time();
+	philo->meals_eaten = 0;
+	philo->write_lock = data->write_lock;
+	return (philo);
+}
+
